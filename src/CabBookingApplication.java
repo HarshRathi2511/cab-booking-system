d5415266ae3ba6a1894a9861c0640a1a4daffee7
Harsh Rathi <rathiharsh2002@gmail.com>
2022-11-29 12:49:39 +0530

    init:cab booking system

---
+++ src/CabBookingApplication.java
@@ -0,0 +1,9 @@
+/**
+ * CabBookingApplication
+ */
+public class CabBookingApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Cab Booking Application");
+    }
+}
