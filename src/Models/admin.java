45335d2f5ed95e6df26f9e601070237fe488c20d
Atish Diwakar <f20212429@pilani.bits-pilani.ac.in>
2022-11-30 12:05:47 +0530

    add:admin class

---
+++ src/Models/admin.java
@@ -0,0 +1,34 @@
+package Models;
+
+public class admin {
+    private String username = "Admin";
+    private String password = "Password";
+    public boolean logincheck = false;
+
+    public void admin_login(String admin, String password) {
+        if (this.username.compareTo(admin) == 0 && this.password.compareTo(password) == 0) {
+            logincheck = true;
+            System.out.println("Welcome");
+
+
+        } else {
+            System.out.println("Error");
+        }
+    }
+    public void tripCost(){
+
+    }
+    public void trip_algo(){
+
+    }
+
+    public void admin_logout() {
+        if (logincheck) {
+            logincheck = false;
+            System.out.println("admin logged out");
+        } else
+            System.out.println("Error");
+
+
+    }
+}
